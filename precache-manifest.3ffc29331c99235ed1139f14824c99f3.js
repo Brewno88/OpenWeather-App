69720ab436786735be614f9e567cc71b1980f2e9
Vincenzo <ilcontevince@gmail.com>
2020-02-28 15:16:26 +0000

    Updates

---
+++ precache-manifest.3ffc29331c99235ed1139f14824c99f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8de80d3e8feb1ccf5744e24f5601ea",
+    "url": "/OpenWeather-App/index.html"
+  },
+  {
+    "revision": "1ec39e1ca553b5c57ac3",
+    "url": "/OpenWeather-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d247c91b641b0e015755",
+    "url": "/OpenWeather-App/static/js/2.0a4312d1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/OpenWeather-App/static/js/2.0a4312d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec39e1ca553b5c57ac3",
+    "url": "/OpenWeather-App/static/js/main.0af9e037.chunk.js"
+  },
+  {
+    "revision": "81c8b26e8d31612e1f91",
+    "url": "/OpenWeather-App/static/js/runtime-main.99f2161e.js"
+  }
+]);
